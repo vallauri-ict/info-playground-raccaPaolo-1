e7992a67e0f15d1578fd2bd1ac2730e263e4b6cc
Paolo Racca <p.racca.1368@vallauri.edu>
2021-09-20 11:21:36 +0200

    [5_002] Upload Ex. 5_002
    Done Ex. 5_002, given as homework.

---
+++ 5_002_Aereoporto/5_002_Aereoporto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_002_Aereoporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 5_002_Aereoporto/query/creazioneDB.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE Aeroporto (
+Citta VARCHAR(30) PRIMARY KEY,
+Nazione VARCHAR(3),
+NumPiste INT)
+
+CREATE TABLE Volo (
+IdVolo INT PRIMARY KEY IDENTITY,
+GiornoSettimana VARCHAR(10),
+CittaPartenza VARCHAR(10),
+OraPartenza TIME,
+CittaArrivo VARCHAR(10),
+OraArrivo TIME,
+TipoAereo VARCHAR(10))
+
+CREATE TABLE Aereo(
+TipoAereo VARCHAR(10) PRIMARY KEY,
+NumPasseggeri INT,
+QtaMerci INT
+)
