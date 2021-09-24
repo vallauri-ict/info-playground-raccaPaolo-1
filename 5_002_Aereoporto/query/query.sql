f57e82990160ff232481795e7625ef97ca156c55
Paolo Racca <p.racca.1368@vallauri.edu>
2021-09-24 10:45:54 +0200

    [Ex. 5_002] Upload query.sql
    Created query.sql file.

---
+++ 5_002_Aereoporto/query/query.sql
@@ -0,0 +1,16 @@
+﻿/*	Città di partenza distinte e in ordine alfabetico	*/
+SELECT DISTINCT v.CittaPartenza
+FROM Volo v
+WHERE v.cittaArrivo = 'Roma'
+ORDER BY v.CittaPartenza;
+
+/*	Città con aereoporto con numero di piste ignoto	*/
+SELECT DISTINCT a.Citta
+FROM Aeroporto a
+WHERE a.NumPiste IS NULL;
+
+/*	Per ogni volo misto (sia merci che passeggeri): codice volo e dati di trasporto	*/
+
+SELECT v.IdVolo, a.QtaMerci, a.NumPasseggeri
+FROM Aereo a, Volo v
+WHERE a.TipoAereo = v.TipoAereo AND a.NumPasseggeri > 0 AND a.QtaMerci > 0;
