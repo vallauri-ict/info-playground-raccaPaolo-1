0982c5d533bc19c18f7986543110d18bc9e80e47
Paolo Racca <p.racca.1368@vallauri.edu>
2021-09-25 10:45:48 +0200

    [Ex.5_003] Upload
    Upload homework Es.01 for the current project: createDB and insertDB.

---
+++ 5_003_Cinema/query/insertDb.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO Attore VALUES
+('Paolo',12,'IT'),
+('Samulele',31,'IT'),
+('Luca',8,'IT')
+
+INSERT INTO Film VALUES
+('Best film ever', 2000, 'IT', 'Racca', 'Giallo', 200)
+
+INSERT INTO Recita VALUES
+(1,1)
+
+INSERT INTO Proiezione VALUES
+(1,1,'20001220',1000)
+
+INSERT INTO Sala VALUES
+(1000,'Vittoria','Stura')
