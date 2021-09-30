39a642b77cd1f31e4c7fa7ad7cc6f1c72e56ac04
Paolo Racca <p.racca.1368@vallauri.edu>
2021-09-30 12:49:50 +0200

    [Ex5_004] Upload exercise
    Upload Ex. 5_004

---
+++ 5_004_Musei/5_004_Musei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_004_Musei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
