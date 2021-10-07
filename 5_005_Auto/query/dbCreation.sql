431144331d95aeaa929e0bba412e9e274eac974a
Paolo Racca <p.racca.1368@vallauri.edu>
2021-10-07 22:30:06 +0200

    [Ex.5_005] Started db creation

---
+++ 5_005_Auto/query/dbCreation.sql
@@ -0,0 +1,77 @@
+﻿DROP TABLE AutoCoinvolta,Automobile,Sinistro,Proprietario,Assicurazione;
+
+CREATE TABLE Proprietario(
+CodF VARCHAR(5) PRIMARY KEY,
+Nome VARCHAR(20),
+Residenza VARCHAR(25)
+)
+
+INSERT INTO Proprietario VALUES
+('RCCPL','Paolo','Cuneo'),
+('DTF','Samuele','Torino'),
+('DLL','Luca','Alba')
+
+UPDATE Proprietario SET
+Nome= 'Antonio'
+WHERE CodF = 'RCCPL'
+
+CREATE TABLE Assicurazione(
+CodAss INT PRIMARY KEY,
+Nome VARCHAR(20),
+Sede VARCHAR(25)
+)
+
+INSERT INTO Assicurazione VALUES
+(1,'Generali','Cuneo'),
+(2,'Toro','Savigliano'),
+(3,'Sara','Savigliano')
+
+UPDATE Assicurazione SET
+Nome='Arancio'
+WHERE CodAss=1
+
+
+CREATE TABLE Automobile(
+Targa VARCHAR(10) PRIMARY KEY,
+Marca VARCHAR(10),
+Cilindrata INT,
+Potenza INT,
+CodF VARCHAR(5),
+CodAss INT,
+FOREIGN KEY (CodF) REFERENCES Proprietario(CodF),
+FOREIGN KEY (CodAss) REFERENCES Assicurazione(CodAss)
+)
+
+INSERT INTO Automobile VALUES 
+('EF999IT','Fiat',2000,150,'RCCPL',3),
+('EL899IT','Audi',1600,90,'DTF',2),
+('GA900IL','Fiat',2000,120,'DLL',1)
+
+
+CREATE TABLE Sinistro(
+CodS INT PRIMARY KEY,
+Localita VARCHAR(20),
+Data DATE
+)
+
+INSERT INTO Sinistro VALUES
+(1,'Alba','2002-01-20'),
+(2,'Cuneo','2021-01-10')
+
+UPDATE Sinistro SET
+Data='2021-02-19'
+WHERE CodS=2
+
+CREATE TABLE AutoCoinvolta(
+CodS INT,
+Targa VARCHAR(10),
+ImportoDelDanno INT,
+PRIMARY KEY(CodS,Targa),
+FOREIGN KEY (CodS) REFERENCES Sinistro(CodS),
+FOREIGN KEY (Targa) REFERENCES Automobile(Targa)
+)
+
+INSERT INTO AutoCoinvolta VALUES
+(1,'EF999IT',250),
+(2,'EL899IT',400),
+(1,'EL899IT',520)
---
+++ 5_005_Auto/query/query.sql
@@ -0,0 +1,13 @@
+﻿SELECT Targa,Marca FROM Automobile
+WHERE Potenza>120 OR Cilindrata>2000;
+
+SELECT Nome,Marca FROM Automobile, Proprietario
+WHERE Proprietario.CodF=Automobile.CodF AND Potenza>120 OR Cilindrata>2000;
+
+SELECT p.Nome,Targa FROM Automobile, Proprietario p, Assicurazione a
+WHERE p.CodF=Automobile.CodF AND Automobile.CodAss=a.CodAss
+AND (Potenza>120 OR Cilindrata>2000) AND a.Nome='Sara';
+
+SELECT DISTINCT p.Nome,au.Targa FROM Automobile au, Proprietario p, Assicurazione a, Sinistro s, AutoCoinvolta
+WHERE p.CodF=au.CodF AND au.CodAss=a.CodAss AND s.CodS=AutoCoinvolta.CodS AND AutoCoinvolta.Targa=au.Targa
+AND s.Data='2002-01-20' AND a.Nome='Sara'
